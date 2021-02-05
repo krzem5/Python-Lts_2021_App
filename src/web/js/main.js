81d78c52a8dc9a89efa156d2ee8b9c43a37c7b01
Krzem <krzem5.dev@gmail.com>
2021-02-05 22:38:52 +0100

    Push Update 02/05/2021, 22:38:43

--- src/web/js/main.js
+++ src/web/js/main.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded",()=>{
-
+	let nme=document.querySelector(".name-inp");
+	let l0e=document.querySelector(".l0");
+	let l1e=document.querySelector(".l1");
+	let se=document.querySelector(".start");
+	let lvl=0;
+	l0e.onclick=()=>{
+		l0e.classList.add("s");
+		l1e.classList.remove("s");
+		lvl=0;
+	}
+	l1e.onclick=()=>{
+		l0e.classList.remove("s");
+		l1e.classList.add("s");
+		lvl=1;
+	}
+	se.onclick=()=>{
+		if (nme.value.length>0){
+			console.log(lvl,nme.value)
+		}
+	}
 },false);
