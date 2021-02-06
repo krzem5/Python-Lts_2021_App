6da44783e959d4ada8fbe2dae596571754c9d68f
Krzem <krzem5.dev@gmail.com>
2021-02-06 09:48:36 +0100

    Push Update 02/06/2021, 09:48:19

--- src/web/js/start.js
+++ src/web/js/start.js
@@ -1,5 +1,8 @@
 document.addEventListener("DOMContentLoaded",()=>{
+	function _render_text(txt){
+		return txt.replace(/__NAME__/gm,window._dt.name).replace(/\*\*((?:[^\*]|\*[^\*])*)\*\*/gm,`<span class="highlight">$1</span>`);
+	}
 	fetch("/api/storyline").then((e)=>e.json()).then((e)=>{
-		document.body.innerHTML+=`window._dt=${JSON.stringify(window._dt)};storyline=${JSON.stringify(e)}`;
+		document.body.innerHTML+=`window._dt = ${JSON.stringify(window._dt)}<br><br>storyline = ${JSON.stringify(e)}<br><br>rendering = ${_render_text(e[0].saving.desc)}`;
 	});
 },false);
